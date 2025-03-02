163bcb0c8e96ddfa0509742c267b0772ffe4e01d
Ian Rodrigues <ianrodrigues2001@hotmail.com>
2025-03-02 20:15:16 -0300

    feat: adding code to instantiate and start bot and configuring project structure

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("Bom dia")
+import discord
+from discord import app_commands
+from config.settings import settings
+
+class Elcreat(discord.Client):
+    def __init__(self):
+        intents = discord.Intents.all()
+        super().__init__(
+            command_prefix=settings.BOT_PREFIX,
+            intents=intents
+        )
+        self.tree = app_commands.CommandTree(self)
+
+    async def setup_hook(self):
+        await self.tree.sync()
+
+    async def on_ready(self):
+        print(f"O Bot {self.user} foi ligado com sucesso!")
+
+bot = Elcreat()
+
+bot.run(settings.BOT_TOKEN)
